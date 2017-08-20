b48add62befe9a37b2ddebc214ded6ac72ceb6aa
Garrett D'Amore <garrett@damore.org>
2017-08-19 22:41:48 -0700

    Various changes for ZeroTier.
    This includes initial state management and endpoint creation.
    This isn't working yet.

--- src/transport/zerotier/zerotier.c
+++ src/transport/zerotier/zerotier.c
@@ -15,11 +15,15 @@
 
 #include "core/nng_impl.h"
 
+#ifndef _WIN32
+#include <unistd.h>
+#endif
+
 #include <ZeroTierOne.h>
 
 // ZeroTier Transport.  This sits on the ZeroTier L2 network, which itself
 // is implemented on top of L3 (mostly UDP).  This requires the 3rd party
-// libzerotiercore library (which is GPL3!) and platform specific UDP
+// libzerotiercore library (which is GPLv3!) and platform specific UDP
 // functionality to be built in.  Note that care must be taken to link
 // dynamically if one wishes to avoid making your entire application GPL3.
 // (Alternatively ZeroTier offers commercial licenses which may prevent
@@ -40,6 +44,87 @@
 //
 typedef struct nni_zt_pipe nni_zt_pipe;
 typedef struct nni_zt_ep   nni_zt_ep;
+typedef struct nni_zt_node nni_zt_node;
+
+// This node structure is wrapped around the ZT_node; this allows us to
+// have multiple endpoints referencing the same ZT_node, but also to
+// support different nodes (identities) based on different homedirs.
+// This means we need to stick these on a global linked list, manage
+// them with a reference count, and uniquely identify them using the
+// homedir.
+
+struct nni_zt_node {
+	char          zn_path[NNG_MAXADDRLEN + 1]; // ought to be sufficient
+	ZT_Node *     zn_znode;
+	nni_list_node zn_link;
+	int           zn_refcnt;
+	nni_list      zn_eps;
+	nni_list      zn_pipes;
+};
+
+struct nni_zt_pipe {
+	nni_list_node znode_link;
+	const char *  addr;
+	uint16_t      peer;
+	uint16_t      proto;
+	size_t        rcvmax;
+	nni_aio *     user_txaio;
+	nni_aio *     user_rxaio;
+	nni_aio *     user_negaio;
+
+	// XXX: fraglist
+	nni_sockaddr remaddr;
+	nni_sockaddr locaddr;
+
+	nni_aio  txaio;
+	nni_aio  rxaio;
+	nni_msg *rxmsg;
+	nni_mtx  mtx;
+};
+
+struct nni_zt_ep {
+	nni_list_node znode_link;
+	char          url[NNG_MAXADDRLEN + 1];
+	char          home[NNG_MAXADDRLEN + 1]; // should be enough
+	ZT_Node *     ztnode;
+	uint64_t      nwid;
+	int           mode;
+	nni_sockaddr  locaddr;
+	int           closed;
+	uint16_t      proto;
+	size_t        rcvmax;
+	nni_aio       aio;
+	nni_aio *     user_aio;
+	nni_mtx       mtx;
+};
+
+static nni_mtx  nni_zt_lk;
+static nni_list nni_zt_nodes;
+
+static int
+nni_zt_result(enum ZT_ResultCode rv)
+{
+	switch (rv) {
+	case ZT_RESULT_OK:
+		return (0);
+	case ZT_RESULT_OK_IGNORED:
+		return (0);
+	case ZT_RESULT_FATAL_ERROR_OUT_OF_MEMORY:
+		return (NNG_ENOMEM);
+	case ZT_RESULT_FATAL_ERROR_DATA_STORE_FAILED:
+		return (NNG_EPERM);
+	case ZT_RESULT_FATAL_ERROR_INTERNAL:
+		return (NNG_EINTERNAL);
+	case ZT_RESULT_ERROR_NETWORK_NOT_FOUND:
+		return (NNG_EADDRINVAL);
+	case ZT_RESULT_ERROR_UNSUPPORTED_OPERATION:
+		return (NNG_ENOTSUP);
+	case ZT_RESULT_ERROR_BAD_PARAMETER:
+		return (NNG_EINVAL);
+	default:
+		return (NNG_ETRANERR + (int) rv);
+	}
+}
 
 // ZeroTier Node API callbacks
 static int
@@ -75,19 +160,113 @@ nni_zt_event_cb(ZT_Node *node, void *userptr, void *threadptr,
 {
 }
 
+static const char *zt_files[] = {
+	// clang-format off
+	NULL, // none, i.e. not used at all
+	"identity.public",
+	"identity.secret",
+	"planet",
+	NULL, // moon, e.g. moons.d/<ID>.moon -- we don't persist it
+	NULL, // peer, e.g. peers.d/<ID> -- we don't persist this
+	NULL, // network, e.g. networks.d/<ID>.conf -- we don't persist
+	// clang-format on
+};
+
+#ifdef _WIN32
+#define unlink DeleteFile
+#define pathsep "\\"
+#else
+#define pathsep "/"
+#endif
+
 static void
 nni_zt_state_put(ZT_Node *node, void *userptr, void *threadptr,
     enum ZT_StateObjectType objtype, const uint64_t objid[2], const void *data,
     int len)
 {
+	FILE *       file;
+	nni_zt_node *ztn = userptr;
+	char         path[NNG_MAXADDRLEN + 1];
+	const char * fname;
+
+	NNI_ARG_UNUSED(objid); // only use global files
+
+	if ((objtype > ZT_STATE_OBJECT_NETWORK_CONFIG) ||
+	    ((fname = zt_files[(int) objtype]) == NULL)) {
+		return;
+	}
+
+	(void) snprintf(
+	    path, sizeof(path), "%s%s%s", ztn->zn_path, pathsep, fname);
+
+	// We assume that everyone can do standard C I/O.
+	// This may be a bad assumption.  If that's the case,
+	// the platform should supply an alternative implementation.
+	// We are also assuming that we don't need to worry about
+	// atomic updates.  As these items (keys, etc.)  pretty much
+	// don't change, this should be fine.
+
+	if (len < 0) {
+		(void) unlink(path);
+		return;
+	}
+
+	if ((file = fopen(path, "wb")) == NULL) {
+		return;
+	}
+
+	if (fwrite(data, 1, len, file) != len) {
+		fclose(file);
+		(void) unlink(path);
+	}
+
+	(void) fclose(file);
 }
 
 static int
-nni_zt_state_get(ZT_Node *node, void *uptr, void *threadptr,
+nni_zt_state_get(ZT_Node *node, void *userptr, void *threadptr,
     enum ZT_StateObjectType objtype, const uint64_t objid[2], void *data,
     unsigned int len)
 {
-	return (-1);
+	FILE *       file;
+	nni_zt_node *ztn = userptr;
+	char         path[NNG_MAXADDRLEN + 1];
+	const char * fname;
+	int          nread;
+
+	NNI_ARG_UNUSED(objid); // only use global files
+
+	if ((objtype > ZT_STATE_OBJECT_NETWORK_CONFIG) ||
+	    ((fname = zt_files[(int) objtype]) == NULL)) {
+		return (-1);
+	}
+
+	(void) snprintf(
+	    path, sizeof(path), "%s%s%s", ztn->zn_path, pathsep, fname);
+
+	// We assume that everyone can do standard C I/O.
+	// This may be a bad assumption.  If that's the case,
+	// the platform should supply an alternative implementation.
+	// We are also assuming that we don't need to worry about
+	// atomic updates.  As these items (keys, etc.)  pretty much
+	// don't change, this should be fine.
+
+	if ((file = fopen(path, "wb")) == NULL) {
+		return (-1);
+	}
+
+	// seek to end of file
+	(void) fseek(file, 0, SEEK_END);
+	if (ftell(file) > len) {
+		fclose(file);
+		return (-1);
+	}
+	(void) fseek(file, 0, SEEK_SET);
+
+	nread = (int) fread(data, 1, len, file);
+	(void) fclose(file);
+
+	return (nread);
 }
 
 // This function is called when ZeroTier desires to send a physical frame.
@@ -115,86 +294,81 @@ static struct ZT_Node_Callbacks nni_zt_callbacks = {
 	.pathLookupFunction           = NULL,
 };
 
-static ZT_Node *nni_zt_node;
+static int
+nni_zt_find_node(nni_zt_node **znp, const char *path)
+{
+	nni_zt_node *      zn;
+	enum ZT_ResultCode zrv;
 
-struct nni_zt_pipe {
-	const char *addr;
-	uint16_t    peer;
-	uint16_t    proto;
-	size_t      rcvmax;
-	nni_aio *   user_txaio;
-	nni_aio *   user_rxaio;
-	nni_aio *   user_negaio;
+	nni_mtx_lock(&nni_zt_lk);
+	NNI_LIST_FOREACH (&nni_zt_nodes, zn) {
+		if (strcmp(path, zn->zn_path) == 0) {
+			zn->zn_refcnt++;
+			*znp = zn;
+			nni_mtx_unlock(&nni_zt_lk);
+			return (0);
+		}
+	}
 
-	// XXX: fraglist
-	nni_sockaddr remaddr;
-	nni_sockaddr locaddr;
+	// We didn't find a node, so make one.  And try to initialize it.
+	if ((zn = NNI_ALLOC_STRUCT(zn)) == NULL) {
+		return (NNG_ENOMEM);
+	}
+	(void) snprintf(zn->zn_path, sizeof(zn->zn_path), "%s", path);
+	zrv = ZT_Node_new(
+	    &zn->zn_znode, zn, NULL, &nni_zt_callbacks, nni_clock() / 1000);
+	if (zrv != ZT_RESULT_OK) {
+		nni_mtx_unlock(&nni_zt_lk);
+		NNI_FREE_STRUCT(zn);
+		return (nni_zt_result(zrv));
+	}
+	zn->zn_refcnt++;
+	*znp = zn;
+	NNI_LIST_INIT(&zn->zn_eps, nni_zt_ep, znode_link);
+	NNI_LIST_INIT(&zn->zn_pipes, nni_zt_pipe, znode_link);
+	nni_mtx_unlock(&nni_zt_lk);
+	return (0);
+}
 
-	nni_aio  txaio;
-	nni_aio  rxaio;
-	nni_msg *rxmsg;
-	nni_mtx  mtx;
-};
-
-struct nni_zt_ep {
-	char         addr[NNG_MAXADDRLEN + 1];
-	nni_sockaddr locaddr;
-	int          closed;
-	uint16_t     proto;
-	size_t       rcvmax;
-	nni_aio      aio;
-	nni_aio *    user_aio;
-	nni_mtx      mtx;
-};
+static void
+nni_zt_rele_node(nni_zt_node *zn)
+{
+	nni_mtx_lock(&nni_zt_lk);
+	zn->zn_refcnt--;
+	if (zn->zn_refcnt == 0) {
+		nni_list_remove(&nni_zt_nodes, zn);
+	}
+	ZT_Node_delete(zn->zn_znode);
+	NNI_FREE_STRUCT(zn);
+	nni_mtx_unlock(&nni_zt_lk);
+}
 
 static int
-nni_zt_result(enum ZT_ResultCode rv)
+nni_zt_chkopt(int opt, const void *dat, size_t sz)
 {
-	switch (rv) {
-	case ZT_RESULT_OK:
-		return (0);
-	case ZT_RESULT_OK_IGNORED:
-		return (0);
-	case ZT_RESULT_FATAL_ERROR_OUT_OF_MEMORY:
-		return (NNG_ENOMEM);
-	case ZT_RESULT_FATAL_ERROR_DATA_STORE_FAILED:
-		return (NNG_EPERM);
-	case ZT_RESULT_FATAL_ERROR_INTERNAL:
-		return (NNG_EINTERNAL);
-	case ZT_RESULT_ERROR_NETWORK_NOT_FOUND:
-		return (NNG_EADDRINVAL);
-	case ZT_RESULT_ERROR_UNSUPPORTED_OPERATION:
-		return (NNG_ENOTSUP);
-	case ZT_RESULT_ERROR_BAD_PARAMETER:
-		return (NNG_EINVAL);
+	switch (opt) {
+	case NNG_OPT_RCVMAXSZ:
+	// case NNG_OPT_TRANSPORT(NNG_TRAN_ZT, xyz):
+	// XXX: We need a way to specify user options, like the path
+	// for the identity files.
 	default:
-		return (NNG_ETRANERR + (int) rv);
+		return (NNG_ENOTSUP);
 	}
 }
+
 static int
 nni_zt_tran_init(void)
 {
-	enum ZT_ResultCode ztrv;
-	// XXX: This needs to set up the initial global listener.  It
-	// quite likely shouldn't actually start listening yet, but
-	// everything should be ready for when the first endpoint listen
-	// comes in.  XXX: What values to use for USERPTR?  Maybe we just
-	// do not care.
-	ztrv = ZT_Node_new(
-	    &nni_zt_node, NULL, NULL, &nni_zt_callbacks, nni_clock());
-	if (ztrv != 0) {
-		return (nni_zt_result(ztrv));
-	}
-	return (NNG_ENOTSUP);
+	nni_mtx_init(&nni_zt_lk);
+	NNI_LIST_INIT(&nni_zt_nodes, nni_zt_node, zn_link);
+	return (0);
 }
 
 static void
 nni_zt_tran_fini(void)
 {
-	if (nni_zt_node != NULL) {
-		ZT_Node_delete(nni_zt_node);
-		nni_zt_node = NULL;
-	}
+	NNI_ASSERT(nni_list_empty(&nni_zt_nodes));
+	nni_mtx_fini(&nni_zt_lk);
 }
 
 static void
@@ -259,7 +433,21 @@ nni_zt_ep_fini(void *arg)
 static int
 nni_zt_ep_init(void **epp, const char *url, nni_sock *sock, int mode)
 {
-	return (NNG_ENOTSUP);
+	nni_zt_ep *ep;
+
+	if ((ep = NNI_ALLOC_STRUCT(ep)) == NULL) {
+		return (NNG_ENOMEM);
+	}
+	// URL parsing...
+	// URL is form zt://<nwid>[/<remoteaddr>]:<port>
+	// The <remoteaddr> part is required for remote dialers, but
+	// is not used at all for listeners.  (We have no notion of binding
+	// to different node addresses.)
+	ep->mode = mode;
+	(void) snprintf(ep->url, sizeof(url), "%s", url);
+	*epp = ep;
+	nni_mtx_init(&ep->mtx);
+	return (0);
 }
 
 static void
@@ -307,18 +495,20 @@ static nni_tran_ep nni_zt_ep_ops = {
 	.ep_bind    = nni_zt_ep_bind,
 	.ep_accept  = nni_zt_ep_accept,
 	.ep_close   = nni_zt_ep_close,
-	.ep_setopt  = NULL,
+	.ep_setopt  = NULL, // XXX: we need the ability to set homepath, etc.
 	.ep_getopt  = NULL,
 };
 
 // This is the ZeroTier transport linkage, and should be the only global
 // symbol in this entire file.
-struct nni_tran nni_zt_tran = {
-	.tran_scheme = "zt",
-	.tran_ep     = &nni_zt_ep_ops,
-	.tran_pipe   = &nni_zt_pipe_ops,
-	.tran_init   = nni_zt_tran_init,
-	.tran_fini   = nni_zt_tran_fini,
+static struct nni_tran nni_zt_tran = {
+	.tran_version = NNI_TRANSPORT_VERSION,
+	.tran_scheme  = "zt",
+	.tran_ep      = &nni_zt_ep_ops,
+	.tran_pipe    = &nni_zt_pipe_ops,
+	.tran_chkopt  = nni_zt_chkopt,
+	.tran_init    = nni_zt_tran_init,
+	.tran_fini    = nni_zt_tran_fini,
 };
 
 #else
